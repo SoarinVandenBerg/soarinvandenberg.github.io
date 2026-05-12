3ef2c40ae7ab1027d880e55e3c32b29bf5ab4b4a
SoarinVandenBerg <svberg07@gmail.com>
2026-05-12 16:55:11 -0700

    adding tabs

--- life.js
+++ life.js
@@ -1,2 +1,22 @@
 const canvas = document.getElementById("life");
 const ctx = canvas.getContext("2d");
+const cols = 30;
+const rows = 20;
+const cellWidth = canvas.width / cols;
+const cellHeight = canvas.height / rows;
+function drawGrid() {
+    ctx.strokeStyle = "#444"
+
+    for (let x = 0; x <= cols; x++) {
+        ctx.beginPath();
+        ctx.moveTo(x * cellWidth, 0);
+        ctx.lineTo(x * cellWidth, canvas.height);
+        ctx.stroke();
+    }
+    for (let y = 0; y <= rows; y++) {
+        ctx.beginPath();
+        ctx.moveTo(0, y * cellHeight);
+        ctx.lineTo(canvas.width, y * cellHeight);
+        ctx.stroke();
+    }
+}
