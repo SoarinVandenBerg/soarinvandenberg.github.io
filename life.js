aaae304cb3f80442b1754f929946b7a1319d7a92
SoarinVandenBerg <svberg07@gmail.com>
2026-05-12 17:25:51 -0700

    good stopping point

--- life.js
+++ life.js
@@ -7,16 +7,34 @@ const cellHeight = canvas.height / rows;
 function drawGrid() {
     ctx.strokeStyle = "#444"
 
-    for (let x = 0; x <= cols; x++) {
+    for (let x = 0; x <= cols; x = x + 1) {
         ctx.beginPath();
         ctx.moveTo(x * cellWidth, 0);
         ctx.lineTo(x * cellWidth, canvas.height);
         ctx.stroke();
     }
-    for (let y = 0; y <= rows; y++) {
+    for (let y = 0; y <= rows; y = y + 1) {
         ctx.beginPath();
         ctx.moveTo(0, y * cellHeight);
         ctx.lineTo(canvas.width, y * cellHeight);
         ctx.stroke();
+    }
+}
+let board;
+function initBoard() {
+    board = [];
+    for (let y = 0; y < rows; y = y + 1) {
+        board [y] = [];
+        for (let x = 0; x < cols; x = x + 1) {
+            board[y][x] = 0;
+        }
+    }
+}
+function drawCells() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let y = 0; y < rows; y = y + 1) {
+        for (let x = 0; x < cols; x = x + 1) {
+
+        }
     }
 }
