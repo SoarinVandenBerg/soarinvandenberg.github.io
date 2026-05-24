48b61780fb3fa1479d36c66d3d23eaaa71ba76e4
SoarinVandenBerg <svberg07@gmail.com>
2026-05-24 12:50:22 -0700

    new countdown!!

--- countdowns.js
+++ countdowns.js
@@ -311,6 +311,29 @@ const now = new Date();
                     `${pad(days)}:${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
     }, 1000);
 
+const Crash = new Date("Febuary 11, 2031 19:45:00");
+    // I know this date isnt exact, becuase we dont really have a date yet, but this is the best one I could find
+
+setInterval(() => {
+              const now3 = new Date();
+              const diff = Crash - now4;
+
+                if (diff <= 0) {
+                    document.getElementById("countdown5").innerText = "BOOOOM";
+                    return;
+                }
+
+                const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+                const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+                const minutes = Math.floor((diff / (1000 * 60)) % 60);
+                const seconds = Math.floor((diff / 1000) % 60);
+
+                const pad = (n) => String(n).padStart(2, "0");
+
+                document.getElementById("countdown5").innerText =
+                    `${pad(days)}:${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+    }, 1000);
+
 const endOfYear = new Date("June 23, 2026 15:10:00");
 
 function countSchoolDaysPrecise(now, end) {
