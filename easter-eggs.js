a1df8906b23d3e6805af658eab3b40de5def1a6c
SoarinVandenBerg <svberg07@gmail.com>
2026-05-26 13:04:32 -0700

    Hidden stuff

---
+++ easter-eggs.js
@@ -0,0 +1,26 @@
+  let totalEggs = 2;
+      let foundEggs = 0;
+
+      if (localStorage.getItem("egg1_found")) {
+        foundEggs++;
+      }
+      else if (localStorage.getItem("egg2_found")) {
+        foundEggs++
+      }
+
+      document.getElementById("eggCount").innerText =
+      `${foundEggs}/${totalEggs} Easter Eggs Found!`;
+
+      let list = document.getElementById("eggList");
+
+        if (localStorage.getItem("egg1_found")) {
+          let item = document.createElement("li");
+          item.innerText = "-The bottom-right button"
+          list.appendChild(item);
+        }
+        
+        if (localStorage.getItem("egg2_found")) {
+          let item = document.createElement("li");
+          item.innerText = "-Visit every page in the site!"
+          list.appendChild(item);
+        }
