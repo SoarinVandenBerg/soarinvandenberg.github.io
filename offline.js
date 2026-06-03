6a1b96337efe739b5acc22671fdf340c91f37b28
SoarinVandenBerg <svberg07@gmail.com>
2026-06-02 18:02:17 -0700

    Updates

--- offline.js
+++ offline.js
@@ -1,12 +1,12 @@
-document.getElementById("nomac").addEventListener("click", function() {
-  localStorage.setItem("allowmac", "true");
-});
-
 document.addEventListener("DOMContentLoaded", () => {
   const ua = navigator.userAgent;
 
   const isIOS = /iPad|iPod/i.test(ua);
   const isMac = /Macintosh/i.test(ua);
+
+  document.getElementById("nomac").addEventListener("click", function() {
+    localStorage.setItem("allowmac", "true");
+  });
 
   if ((isIOS || isMac) && !localStorage.getItem("allowmac")) {
     window.location.href = "/offline";
