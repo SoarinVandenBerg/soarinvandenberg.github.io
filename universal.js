1dd90643863b5b6b63b766b810b86d36d8fcadab
SoarinVandenBerg <svberg07@gmail.com>
2026-06-05 14:42:16 -0700

    all code for all pages

--- universal.js
+++ universal.js
@@ -46,10 +46,10 @@ document.addEventListener("keydown", (e) => {
       }
  });
 
-    function openEggPopup(eggkey) {
-        if (!localStorage.getItem(eggkey)) {
-            localStorage.setItem(eggkey, "true");
-        };
+function openEggPopup(eggkey) {
+    if (!localStorage.getItem(eggkey)) {
+        localStorage.setItem(eggkey, "true");
+    };
 
     const overlay = document.querySelector(".background");
     const yesBtn = document.querySelector("#eggPopupYes");
@@ -67,62 +67,112 @@ document.addEventListener("keydown", (e) => {
 }
 
 const sitePages = [
-          "visited_about",
-          "visited_ai",
-          "visited_notes",
-          "visited_books",
-          "visited_contact",
-          "visited_countdowns",
-          "visited_index",
-          "visited_pages",
-          "visited_projects",
-          "visited_guestbook",
-        ];
+    "visited_about",
+    "visited_ai",
+    "visited_notes",
+    "visited_books",
+    "visited_contact",
+    "visited_countdowns",
+    "visited_index",
+    "visited_pages",
+    "visited_projects",
+    "visited_guestbook",
+];
 
-        let allSiteVisited = sitePages.every(key => localStorage.getItem(key));
-        if (allSiteVisited && !localStorage.getItem("egg2_found")) {
-          localStorage.setItem("egg2_found", "true");
-          openEggPopup("egg2_found");
-        }
+let allSiteVisited = sitePages.every(key => localStorage.getItem(key));
+if (allSiteVisited && !localStorage.getItem("egg2_found")) {
+  localStorage.setItem("egg2_found", "true");
+  openEggPopup("egg2_found");
+}
 
-        const countdownPages = [
-          "count1",
-          "count2",
-          "count3",
-          "count4",
-          "count5",
-          "count6",
-        ];
+const countdownPages = [
+  "count1",
+  "count2",
+  "count3",
+  "count4",
+  "count5",
+  "count6",
+];
 
-        const allEggs = [
-          "egg1_found",
-          "egg2_found",
-          "egg3_found",
-          "egg4_found",
-          "egg5_found",
-          "egg6_found",
-          "egg8_found",
-          "egg9_found",
-          "egg10_found",
-          "egg11_found",
-        ]
+const allEggs = [
+    "egg1_found",
+    "egg2_found",
+    "egg3_found",
+    "egg4_found",
+    "egg5_found",
+    "egg6_found",
+    "egg8_found",
+    "egg9_found",
+    "egg10_found",
+    "egg11_found",
+]
 
-        let allCountVisited = countdownPages.every(key => localStorage.getItem(key));
-        if (allCountVisited && !localStorage.getItem("egg3_found")) {
-          localStorage.setItem("egg3_found", "true");
-          openEggPopup("egg3_found");
-        }
+let allCountVisited = countdownPages.every(key => localStorage.getItem(key));
+if (allCountVisited && !localStorage.getItem("egg3_found")) {
+  localStorage.setItem("egg3_found", "true");
+  openEggPopup("egg3_found");
+}
 
-        const brokenPage = ["visited_404"];
+const brokenPage = ["visited_404"];
 
-        let brokenVisited = brokenPage.every(key => localStorage.getItem(key));
-        if (brokenVisited && !localStorage.getItem("egg6_found")) {
-          localStorage.setItem("egg6_found", "true");
-          openEggPopup("egg6_found");
-        }
+let brokenVisited = brokenPage.every(key => localStorage.getItem(key));
+if (brokenVisited && !localStorage.getItem("egg6_found")) {
+  localStorage.setItem("egg6_found", "true");
+  openEggPopup("egg6_found");
+}
       
-        let allEggs1 = allEggs.every(key => localStorage.getItem(key));
-        if (allEggs1 && !localStorage.getItem("egg7_found")) {
-          localStorage.setItem("egg7_found", "true");
-          openEggPopup("egg7_found");
+let allEggs1 = allEggs.every(key => localStorage.getItem(key));
+if (allEggs1 && !localStorage.getItem("egg7_found")) {
+  localStorage.setItem("egg7_found", "true");
+  openEggPopup("egg7_found");
+}
+
+const cheatCode = [
+    "1", "3",
+    "2", "4",
+    "3", "5",
+];
+
+let cheatPosition = 0;
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === cheatCode[cheatPosition]) {
+        cheatPosition++;
+
+        if (cheatPosition === cheatCode.length) {
+            localStorage.setItem("egg1_found", "true");
+            localStorage.setItem("egg2_found", "true");
+            localStorage.setItem("egg3_found", "true");
+            localStorage.setItem("egg4_found", "true");
+            localStorage.setItem("egg5_found", "true");
+            localStorage.setItem("egg6_found", "true");
+            localStorage.setItem("egg7_found", "true");
+            localStorage.setItem("egg8_found", "true");
+            localStorage.setItem("egg9_found", "true");
+            localStorage.setItem("egg10_found", "true");
+            localStorage.setItem("egg11_found", "true");
+            localStorage.setItem("visits_index", "true");
+            localStorage.setItem("visited_about", "true");
+            localStorage.setItem("visited_ai", "true");
+            localStorage.setItem("visited_notes", "true");
+            localStorage.setItem("visited_books", "true");
+            localStorage.setItem("visited_contact", "true");
+            localStorage.setItem("visited_guestbook", "true");
+            localStorage.setItem("visited_countdowns", "true");
+            localStorage.setItem("visited_index", "true");
+            localStorage.setItem("visited_pages", "true");
+            localStorage.setItem("visited_projects", "true");
+            localStorage.setItem("visited_404", "true");
+            localStorage.setItem("count1", "true");
+            localStorage.setItem("count2", "true");
+            localStorage.setItem("count3", "true");
+            localStorage.setItem("count4", "true");
+            localStorage.setItem("count5", "true");
+            localStorage.setItem("count6", "true");
+                
+            cheatPosition = 0;
         }
+    } else {
+        cheatPosition = 0;
+      }
+ });
