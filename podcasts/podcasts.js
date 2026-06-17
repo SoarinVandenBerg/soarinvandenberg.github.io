ba8a2817c0fc1416a82d7d73a044bb82817b0e68
SoarinVandenBerg <svberg07@gmail.com>
2026-06-16 19:32:44 -0700

    testing number 2

--- podcasts/podcasts.js
+++ podcasts/podcasts.js
@@ -4,9 +4,9 @@ const API_KEY = "AIzaSyD1zP0euN0zVChL9i8rSumRRZmwIcRKywo";
 const POD_HUMANS_CHANNEL_ID = "UC7QrGBQ7DGv47UXJobHmL1Q";
 const POD_WAVEFORM_CHANNEL_ID = "UCEcrRXW3oEYfUctetZTAWLw";
 const POD_DEARHANKANDJOHN_CHANNEL_ID = "UCldoL0vMV7hkUPIauUY4IjA";
-const POD_HUMANS_CHANNEL_ID1 = "UU7QrGBQ7DGv47UXJobHmL1Q";
-const POD_WAVEFORM_CHANNEL_ID1 = "UUEcrRXW3oEYfUctetZTAWLw";
-const POD_DEARHANKANDJOHN_CHANNEL_ID1 = "UUldoL0vMV7hkUPIauUY4IjA";
+// const POD_HUMANS_CHANNEL_ID1 = "UU7QrGBQ7DGv47UXJobHmL1Q";
+// const POD_WAVEFORM_CHANNEL_ID1 = "UUEcrRXW3oEYfUctetZTAWLw";
+// const POD_DEARHANKANDJOHN_CHANNEL_ID1 = "UUldoL0vMV7hkUPIauUY4IjA";
 
 async function getLatestHumanVideo() {
   const url =
@@ -66,14 +66,16 @@ async function getLatestDearHankVideo() {
 }
 
 
-async function getLatestTwelveHumans() {
+//Vlogbrothers latest 12 code
+
+const HUMANS_UPLOADS = "UU7QrGBQ7DGv47UXJobHmL1Q";
+
+async function getLatestHumansTwelve() {
   const url =
-    `https://www.googleapis.com/youtube/v3/search?` +
+    `https://www.googleapis.com/youtube/v3/playlistItems?` +
     `key=${API_KEY}` +
-    `&channelId=${POD_HUMANS_CHANNEL_ID}` +
-    `&order=date` +
+    `&playlistId=${HUMANS_UPLOADS}` +
     `&part=snippet` +
-    `&type=video` +
     `&maxResults=12`;
 
   const response = await fetch(url);
@@ -81,62 +83,28 @@ async function getLatestTwelveHumans() {
 
   if (!data.items || data.items.length === 0) return;
 
-  const container = document.getElementById("human-list");
+  const container = document.getElementById("humans-list");
   container.innerHTML = "";
 
   data.items.forEach(item => {
-    const videoId = item.id.videoId;
+    const videoId = item.snippet.resourceId.videoId;
 
     const iframe = document.createElement("iframe");
     iframe.width = "560";
     iframe.height = "315";
     iframe.allowFullscreen = true;
     iframe.style = "border:2px solid #00be00;border-radius:4px;margin-bottom:20px;";
-    iframe.src = `https://www.youtube.com/embed/${videoId}`;
+    iframe.src = `https://yout-ube.com/watch?v=${videoId}`;
 
     container.appendChild(iframe);
   });
 }
 
 
-
-async function getLatestTwelveWaveform() {
-  const url =
-    `https://www.googleapis.com/youtube/v3/search?` +
-    `key=${API_KEY}` +
-    `&channelId=${POD_WAVEFORM_CHANNEL_ID}` +
-    `&order=date` +
-    `&part=snippet` +
-    `&type=video` +
-    `&maxResults=12`;
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  if (!data.items || data.items.length === 0) return;
-
-  const container = document.getElementById("waveform-list");
-  container.innerHTML = "";
-
-  data.items.forEach(item => {
-    const videoId = item.id.videoId;
-
-    const iframe = document.createElement("iframe");
-    iframe.width = "560";
-    iframe.height = "315";
-    iframe.allowFullscreen = true;
-    iframe.style = "border:2px solid #00be00;border-radius:4px;margin-bottom:20px;";
-    iframe.src = `https://www.youtube.com/embed/${videoId}`;
-
-    container.appendChild(iframe);
-  });
-}
+if (document.getElementById("humans-list")) getLatestHumansTwelve();
 
 
 
 if (document.getElementById("waveformvideo")) getLatestWaveformVideo();
 if (document.getElementById("dearhankvideo")) getLatestDearHankVideo();
 if (document.getElementById("humanvideo")) getLatestHumanVideo();
-if (document.getElementById("humans-list")) getLatestTwelveHumans();
-if (document.getElementById("dearhank-list")) getLatestTwelveDearHank();
-if (document.getElementById("waveform-list")) getLatestTwelveWaveform();
--- videos/video-finder.js
+++ videos/video-finder.js
@@ -50,7 +50,7 @@ async function getLatestVlogVideo() {
 
 //Vlogbrothers latest 12 code
 
-const VLOG_UPLOADS = "UU7QrGBQ7DGv47UXJobHmL1Q";
+const VLOG_UPLOADS = "UUGaVdbSav8xWuFWTadK6loA";
 
 async function getLatestVlogTwelve() {
   const url =
